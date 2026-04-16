43b9fdf892c7c61ec1af43062871cefe4b9ba655
meet-gg <meetgoyanip@gmail.com>
2026-04-16 08:42:02 +0530

    fix yaml file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 const port = 8080;
 app.get('/', (req, res) => {
-  res.send('Hello World from ec2!');
+  res.send('Hello World!');
 });  
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
