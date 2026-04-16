fa4d489eaa42b538a5ec4e8681f08499b288a156
meet-gg <meetgoyanip@gmail.com>
2026-04-16 08:42:40 +0530

    chng response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 const port = 8080;
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World from ec2!');
 });  
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
